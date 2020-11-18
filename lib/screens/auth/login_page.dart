a24c259d56d1ed0cddda55d474c224d79b58a351
Andre Freitas da Silva <proandre21@hotmail.com>
2020-11-17 21:38:03 -0300

    02 - 02 - Tela de Login no Flutter - Parte 02

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,18 +1,133 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key key}) : super(key: key);
+  double _deviceWidth;
+  double _deviceHeigth;
 
   @override
   Widget build(BuildContext context) {
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _deviceHeigth = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(
-        child: Text(
-          'Login',
-          style: TextStyle(color: Theme.of(context).primaryColor),
+      body: SingleChildScrollView(
+        child: Center(
+          child: _loginPageUI(context),
         ),
       ),
+    );
+  }
+
+  Widget _loginPageUI(context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.10),
+      child: Column(
+        children: <Widget>[
+          Container(height: 50),
+          _headingAuth(),
+          Container(height: 15),
+          _formLogin(context),
+          Container(height: 15),
+          _loginButton(context),
+          Container(height: 30),
+          _textRegister(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingAuth() {
+    return Container(
+      height: 130,
+      width: 130,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/IconeFlutterFood.png'),
+              fit: BoxFit.fill)),
+    );
+  }
+
+  Widget _formLogin(context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(143, 148, 251, .6),
+            blurRadius: 20,
+            offset: Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          _emailTextField(context),
+          _passowordTextField(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _loginButton(context) {
+    return Container(
+      width: _deviceWidth,
+      child: MaterialButton(
+        onPressed: () {
+          print('object');
+        },
+        color: Theme.of(context).primaryColor,
+        child: Text('LOGIN'),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+    );
+  }
+
+  Widget _emailTextField(context) {
+    return TextFormField(
+      autocorrect: false,
+      autofocus: true,
+      style: TextStyle(color: Theme.of(context).primaryColor),
+      cursorColor: Theme.of(context).primaryColor,
+      decoration: InputDecoration(
+          border: UnderlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+          contentPadding: EdgeInsets.all(10),
+          hintText: 'E-mail',
+          hintStyle: TextStyle(color: Theme.of(context).primaryColor)),
+    );
+  }
+
+  Widget _passowordTextField(context) {
+    return TextFormField(
+      autocorrect: false,
+      autofocus: true,
+      obscureText: true,
+      style: TextStyle(color: Theme.of(context).primaryColor),
+      cursorColor: Theme.of(context).primaryColor,
+      decoration: InputDecoration(
+          border: UnderlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          contentPadding: EdgeInsets.all(10),
+          hintText: 'Passowrd',
+          hintStyle: TextStyle(color: Theme.of(context).primaryColor)),
+    );
+  }
+
+  Widget _textRegister(context) {
+    return GestureDetector(
+      onDoubleTap: () {
+        print('redirecionar...');
+      },
+      child: Text('Cadastrar-se',
+          style: TextStyle(color: Theme.of(context).primaryColor)),
     );
   }
 }
